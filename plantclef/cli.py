b19a2dc2a9ad776b3438196920b744a05388252f
Murilo Gustineli <murilogustineli@gmail.com>
2025-02-19 19:12:05 -0500

    updated top-species-subset sbatcha nd python files

--- plantclef/cli.py
+++ plantclef/cli.py
@@ -1,5 +1,7 @@
 from typer import Typer
 from plantclef.embedding import app as embedding_app
+from plantclef.preprocessing import app as preprocessing_app
 
 app = Typer()
 app.add_typer(embedding_app, name="embedding")
+app.add_typer(preprocessing_app, name="preprocessing")
