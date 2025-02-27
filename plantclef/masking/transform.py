fixed masking merge bug

plantclef/masking/transform.py
@@ -81,8 +81,6 @@ class WrappedMasking(
         ]
         self.BOX_THRESHOLD = 0.15
         self.TEXT_THRESHOLD = 0.1
-        self.INCLUDE_CLASS_IDS = {0, 1, 2}
-        self.INITIALIZED = True
 
     def _nvidia_smi(self):
         from subprocess import run, PIPE
@@ -147,27 +145,24 @@ class WrappedMasking(
         )
         return self._refine_masks(masks[0])
 
-    def merge_masks(
+    def merge_class_masks(
         self, masks: np.ndarray, text_labels: list[str], empty_shape: tuple
     ) -> tuple:
         """Merges masks for each class and prepares the output dictionary."""
-
+        # create empty np arrays for empty masks
         class_masks = {
-            key: np.zeros(empty_shape, dtype=np.uint8)
-            for key in ["leaf", "flower", "plant"]
+            key: np.zeros(empty_shape, dtype=np.uint8) for key in self.CLASSES
         }
 
+        # return all masks
         for text_label, mask in zip(text_labels, masks):
             for key in class_masks.keys():
                 if key not in text_label:
                     continue
+                # merge multiple masks into a single mask
                 class_masks[key] |= mask
 
-        final_mask = np.zeros(empty_shape, dtype=np.uint8)
-        for _, mask in class_masks.items():
-            final_mask |= mask
-
-        return final_mask, class_masks
+        return class_masks
 
     def mask_to_bytes(self, image_array: np.ndarray) -> bytes:
         """Encode the numpy mask array as raw bytes using np.save()."""
@@ -188,12 +183,11 @@ class WrappedMasking(
             input_boxes = self.convert_boxes_to_tensor(detections)
             masks = self.segment(image, input_boxes=input_boxes)
 
-            final_mask, class_masks = self.merge_masks(
+            class_masks = self.merge_class_masks(
                 masks, detections["text_labels"], (image.height, image.width)
             )
 
             return {
-                "combined_mask": self.mask_to_bytes(final_mask),
                 **{f"{k}_mask": self.mask_to_bytes(v) for k, v in class_masks.items()},
             }
 
@@ -204,10 +198,16 @@ class WrappedMasking(
             self._make_predict_fn(),
             returnType=StructType(
                 [
-                    StructField("combined_mask", BinaryType(), False),
                     StructField("leaf_mask", BinaryType(), False),
                     StructField("flower_mask", BinaryType(), False),
                     StructField("plant_mask", BinaryType(), False),
+                    StructField("sand_mask", BinaryType(), False),
+                    StructField("wood_mask", BinaryType(), False),
+                    StructField("stone_mask", BinaryType(), False),
+                    StructField("tape_mask", BinaryType(), False),
+                    StructField("tree_mask", BinaryType(), False),
+                    StructField("rock_mask", BinaryType(), False),
+                    StructField("vegetation_mask", BinaryType(), False),
                 ]
             ),
         )

plantclef/masking/workflow.py
@@ -9,11 +9,6 @@ from pyspark.sql import functions as F
 
 from plantclef.masking.transform import WrappedMasking
 from plantclef.spark import spark_resource
-from plantclef.model_setup import (
-    setup_segment_anything_checkpoint_path,
-    setup_groundingdino_checkpoint_path,
-    setup_groundingdino_config_path,
-)
 
 
 class ProcessMasking(luigi.Task):
@@ -72,10 +67,16 @@ class ProcessMasking(luigi.Task):
                 transformed = transformed.withColumn(c, vector_to_array(F.col(c)))
 
         transformed = (
-            transformed.withColumn("combined_mask", F.col("masks.combined_mask"))
-            .withColumn("leaf_mask", F.col("masks.leaf_mask"))
+            transformed.withColumn("leaf_mask", F.col("masks.leaf_mask"))
             .withColumn("flower_mask", F.col("masks.flower_mask"))
             .withColumn("plant_mask", F.col("masks.plant_mask"))
+            .withColumn("sand_mask", F.col("masks.sand_mask"))
+            .withColumn("wood_mask", F.col("masks.wood_mask"))
+            .withColumn("stone_mask", F.col("masks.stone_mask"))
+            .withColumn("tape_mask", F.col("masks.tape_mask"))
+            .withColumn("tree_mask", F.col("masks.tree_mask"))
+            .withColumn("rock_mask", F.col("masks.rock_mask"))
+            .withColumn("vegetation_mask", F.col("masks.vegetation_mask"))
             .drop("masks")
         )
 

plantclef/plotting.py
@@ -196,8 +196,9 @@ def plot_masks_from_binary(
 
 def plot_individual_masks_comparison(
     joined_df,
-    label_col: str,
-    grid_size=(3, 5),
+    mask_names: list = ["leaf_mask", "flower_mask", "rock_mask"],
+    label_col: str = "image_name",
+    num_rows: int = 3,
     crop_square: bool = False,
     figsize: tuple = (15, 10),
     fontsize: int = 16,
@@ -208,6 +209,7 @@ def plot_individual_masks_comparison(
     Display masks in a grid with image names as labels.
 
     :param joined_df: DataFrame with the original and masked data.
+    :param mask_names: List of mask names to plot individually.
     :param label_col: Name of the species being displayed as image labels.
     :param grid_size: Tuple (rows, cols) representing the grid size.
     :param crop_square: Boolean, whether to crop images to a square format by taking the center.
@@ -215,16 +217,16 @@ def plot_individual_masks_comparison(
     :param dpi: Dots Per Inch, determines the resolution of the output image.
     """
     # Unpack the number of rows and columns for the grid
-    rows, cols = grid_size
+    cols = len(mask_names) + 1
 
     # Collect binary image data from DataFrame
-    subset_df = joined_df.limit(rows).collect()
+    subset_df = joined_df.limit(num_rows).collect()
 
     # Create subplots for image and masks
-    fig, axes = plt.subplots(rows, cols, figsize=figsize, dpi=dpi)
+    fig, axes = plt.subplots(num_rows, cols, figsize=figsize, dpi=dpi)
 
     # Ensure axes is always 2D for consistent iteration
-    axes = axes.reshape(rows, cols)
+    axes = axes.reshape(num_rows, cols)
 
     for row_idx, row in enumerate(subset_df):
         # Load original image
@@ -232,7 +234,6 @@ def plot_individual_masks_comparison(
         image_array = np.array(image)
 
         # Load masks
-        mask_names = ["leaf_mask", "flower_mask", "plant_mask", "combined_mask"]
         masks = [np.load(io.BytesIO(row[mask])) for mask in mask_names]
 
         # Expand masks to match image dimensions
@@ -246,7 +247,7 @@ def plot_individual_masks_comparison(
 
         # Plot each mask
         for col_idx, (mask, mask_name) in enumerate(zip(masks, mask_names), start=1):
-            masked_image = image_array * mask
+            masked_image = image_array * (mask * 255)
             axes[row_idx, col_idx].imshow(masked_image.astype(np.uint8))
             name = mask_name.replace("_", " ").title()
             wrap_mask_name = "\n".join(textwrap.wrap(name, width=wrap_width))