aacb6eccdd6184a507a412985b2d21d98b4c909b
Murilo Gustineli <murilogustineli@gmail.com>
2025-03-01 14:31:23 -0500

    updated retrieval-embed-transform pytest

--- tests/retrieval/test_retrieval_embed_transform.py
+++ tests/retrieval/test_retrieval_embed_transform.py
@@ -12,8 +12,8 @@ from plantclef.model_setup import setup_fine_tuned_model
 )
 def test_embedder_finetuned_dinov2(spark_df, model_name, expected_dim):
     model = EmbedderFineTunedDINOv2(
-        input_col="img",
-        output_col="transformed",
+        input_col="data",
+        output_col="cls_embedding",
         model_path=setup_fine_tuned_model(),
         model_name=model_name,
         batch_size=2,
@@ -24,13 +24,13 @@ def test_embedder_finetuned_dinov2(spark_df, model_name, expected_dim):
     transformed.printSchema()
     transformed.show()
 
-    assert transformed.count() == 2 * 3 * 3
-    assert transformed.columns == ["tile", "transformed"]
+    assert transformed.count() == 3 * 3  # one image, 3x3 grid size
+    assert transformed.columns == ["image_name", "tile", "cls_embedding"]
 
     tile_values = set([row.tile for row in transformed.select("tile").collect()])
     assert tile_values == set(range(3 * 3))
 
-    row = transformed.select("transformed").first()
-    assert isinstance(row.transformed, list)
-    assert len(row.transformed) == expected_dim
-    assert all(isinstance(x, float) for x in row.transformed)
+    row = transformed.select("cls_embedding").first()
+    assert isinstance(row.cls_embedding, list)
+    assert len(row.cls_embedding) == expected_dim
+    assert all(isinstance(x, float) for x in row.cls_embedding)
