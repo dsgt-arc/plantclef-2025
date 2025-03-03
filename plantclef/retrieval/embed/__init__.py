a01c10605b60f63b8f43b46162cf2ef1a5ac36df
Anthony Miyaguchi <acmiyaguchi@gatech.edu>
2025-03-03 02:42:16 -0500

    Add workflow for adding masks

--- plantclef/retrieval/embed/__init__.py
+++ plantclef/retrieval/embed/__init__.py
@@ -1,5 +1,7 @@
 import typer
 from .workflow import main as workflow_main
+from .mask.workflow import filter_by_mask
 
 app = typer.Typer()
 app.command("workflow")(workflow_main)
+app.command("filter-by-mask")(filter_by_mask)
