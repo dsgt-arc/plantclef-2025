267829edf25a1b2aa29e1f79fb7b535bad21a2cc
Murilo Gustineli <murilogustineli@gmail.com>
2025-04-06 21:27:30 -0400

    added naive-baseline notebook, python file and ondemand sh file

--- plantclef/classification/__init__.py
+++ plantclef/classification/__init__.py
@@ -1,7 +1,9 @@
 import typer
 from .workflow import main as workflow_main
 from .aggregation import main as aggregation_main
+from .naive_baseline import main as naive_baseline_main
 
 app = typer.Typer()
 app.command("workflow")(workflow_main)
 app.command("aggregation")(aggregation_main)
+app.command("naive_baseline")(naive_baseline_main)
